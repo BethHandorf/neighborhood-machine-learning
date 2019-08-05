2a37735b299998d9b045bf045a440edad27346ea
BethHandorf <53494370+BethHandorf@users.noreply.github.com>
2019-08-05 17:23:56 -0400

    Update bfn_lso_bin_tt.R

--- Simulation code/bfn_lso_bin_tt.R
+++ Simulation code/bfn_lso_bin_tt.R
@@ -5,7 +5,6 @@ args=(commandArgs(TRUE))
 
 ##args is now a list of character vectors
 ## First check to see if arguments are passed.
-## Then cycle through each element of the list and evaluate the expressions.
 if(length(args)==0){
   print("No arguments supplied.")
   ##supply default values
